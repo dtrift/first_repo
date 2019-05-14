d39395820801a36e52614255c910552bb18e171c
trift <d.trift@mail.ru>
2019-05-14 22:29:58 +0500

    lesson1

---
+++ Desktop/home_work/1/area_triangle.rb
@@ -0,0 +1,12 @@
+puts "Введите основание треугольника (см):"
+base = Float(gets.chomp)
+
+puts "Введитете высоту треугольника (см):"
+height = Float(gets.chomp)
+
+
+area_triangle = 0.5 * (base * height)
+
+puts "Площадь треугольника равна #{area_triangle} кв см"
+
+
