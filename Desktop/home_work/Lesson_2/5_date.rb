834a2ed79c77ac5724caf846852a766d3373d144
dtrift <89174579997@mail.ru>
2019-05-22 22:14:03 +0500

    Lesson_2

--- Desktop/home_work/Lesson_2/5_date.rb
+++ Desktop/home_work/Lesson_2/5_date.rb
@@ -1,16 +1,16 @@
 months = { 
-           '1' => 31 ,
-           '2' => 28,
-           '3' => 31,
-           '4' => 30,
-           '5' => 31,
-           '6' => 30,
-           '7' => 31,
-           '8' => 31,
-           '9' => 30,
-           '10' => 31,
-           '11' => 30,
-           '12' => 31 
+           1 => 31 ,
+           2 => 28,
+           3 => 31,
+           4 => 30,
+           5 => 31,
+           6 => 30,
+           7 => 31,
+           8 => 31,
+           9 => 30,
+           10 => 31,
+           11 => 30,
+           12 => 31 
                        }
 months_leap = months 
 number = 0
@@ -27,17 +27,16 @@ year = gets.chomp.to_i
 t = Time.new(year, month, day)
 puts t
 
-if year % 4 == 0 && year % 100 != 0 #&& year % 400 == 0 
+if year % 4 == 0 && year % 100 != 0
   puts "#{ year } високосный год"
-  months_leap.store("2", 29)
+  months_leap.store(2, 29)
   puts months_leap
-  months_leap.each do |mon, da| 
-    if month == months_leap[da]
-    number = number + months_leap[da]
-    puts "Порядковый номер даты #{ number }"
-
-    else number = number + months[da]
+  months_leap.each do |m, d| 
+    if month == months_leap[d]
+    number += months_leap[d]
     puts "Порядковый номер даты #{ number }"
     end
   end
+else number += months[d]
+puts "Порядковый номер даты #{ number }"  
 end
