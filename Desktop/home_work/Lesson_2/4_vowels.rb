25474c144842f477919d678873d2204417c1972a
dtrift <89174579997@mail.ru>
2019-05-22 23:21:24 +0500

    Lesson_2

--- Desktop/home_work/Lesson_2/4_vowels.rb
+++ Desktop/home_work/Lesson_2/4_vowels.rb
@@ -2,9 +2,9 @@ vowels = ["a", "e", "i", "o", "u", "y"]
 hash_vowels = {}
 
 ("a".."z").each_with_index do |let, index| 
-  if vowels.each { |x| x } == let  
+  if vowels.find { |x| x == let } == let  
     index += 1
     hash_vowels.store(let, index)
-    puts hash_vowels
   end
 end
+puts hash_vowels
