1ff21682884c5a9ac9fa8a3ab8d4f25fe08a5d05
dtrift <89174579997@mail.ru>
2019-05-25 21:05:25 +0500

    Lesson_2

--- Desktop/home_work/Lesson_2/5_date.rb
+++ Desktop/home_work/Lesson_2/5_date.rb
@@ -1,4 +1,4 @@
- months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
  
 puts "Введите день: " 
 day = gets.chomp.to_i 
@@ -13,10 +13,6 @@ if year % 4 == 0 && year % 100 != 0
   puts "#{ year } високосный год"
   months[1] = 29 
 end 
- 
-if month > 1
-  a = months.take(month - 1).sum
-  puts "Порядковый номер даты #{ day }.#{ month }.#{ year } - #{ a + day }"
-  else
-  puts "Порядковый номер даты #{ day }.#{ month }.#{ year } - #{ day }"
-end
+
+a = months.take(month - 1).sum
+puts "Порядковый номер даты #{ day }.#{ month }.#{ year } - #{ a + day }"
