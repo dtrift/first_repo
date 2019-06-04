2e8ff77a01ff51f9e861fbe11de2be0b291eced0
dtrift <89174579997@mail.ru>
2019-06-05 00:25:51 +0500

    Lesson_3

--- Desktop/home_work/Lesson_3/route.rb
+++ Desktop/home_work/Lesson_3/route.rb
@@ -1,5 +1,5 @@
 class Route  
-  attr_reader :name, :first_station, :last_station, :stations
+  attr_reader :first_station, :last_station, :stations
 
   def initialize(first_station, last_station)
     @stations = [first_station, last_station]
--- Desktop/home_work/Lesson_3/train.rb
+++ Desktop/home_work/Lesson_3/train.rb
@@ -25,13 +25,34 @@ class Train
   def get_route(route)
     @train_route = route
     route.first_station.get_train(self)
+    @train_route.stations.each do |station|
+      if station.trains.include?(self)
+        @current_station = station.name 
+      end
+    end
   end
-    
-  def move_forward
-
+  
+=begin  
+  def get_current_station
+    @train_route.stations.each do |station|
+      if station.trains.include?(self)
+        @current_station = station 
+      end
+    end
+    #@current_station = station if @train_route.stations.include?(self)
+    #@current_station = station if station.trains.include?(self)
+    puts @current_station
   end
+=end
 
-  def move_back
+  def move_forward(station)
+    @current_station = @train_route.stations[index + 1]
+    #@previos_station = @current_station index -1
+    #@next_station = @current_station index + 1
+    #puts @current_station
+  end
+  
+  def move_back(station)
   end
   
 end
