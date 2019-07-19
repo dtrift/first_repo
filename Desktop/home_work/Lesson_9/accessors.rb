40946d597bd8b303253f0dc63e5ef907295a4d23
dtrift <89174579997@mail.ru>
2019-07-19 22:39:27 +0500

    Lesson_9_2

--- Desktop/home_work/Lesson_9/accessors.rb
+++ Desktop/home_work/Lesson_9/accessors.rb
@@ -1,14 +1,34 @@
 module Accessors
-  def attr_accessor_with_history(*names)
-    names.each do |name|
-      var_name = "@#{name}".to_sym
-      define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+
+    def attr_accessor_with_history(*names)
+      names.each do |name|
+        var_name = "@#{name}".to_sym
+        var_name_history = "#{name}"
+        var_name_history_values = []
+        define_method(name) { instance_variable_get(var_name) }
+        define_method("#{name}=".to_sym) do |value|
+          instance_variable_set(var_name, value)
+          var_name_history_values << value
+        end
+      end
     end
-  end
 
-  def strong_attr_accessor
-    
-  end
+    def strong_attr_accessor(имя_атрибута, класс_атрибута)
+      
+    end
 
+  module InstanceMethods 
+
+    def "#{var_name_history}_history"
+      var_name_history_values
+    end
+
+  end
 end
