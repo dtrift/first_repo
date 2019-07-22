33843c031dc7fd8bcb982424d2d16257d4ccb30b
dtrift <89174579997@mail.ru>
2019-07-22 06:28:13 +0500

    Lesson_9_3

--- Desktop/home_work/Lesson_9/accessors.rb
+++ Desktop/home_work/Lesson_9/accessors.rb
@@ -12,7 +12,7 @@ module Accessors
     def attr_accessor_with_history(*names)
       names.each do |name|
         var_name = "@#{name}".to_sym
-        @method_name = "#{name}"
+        @method_name = "#{name}".to_s
         define_method(name) { instance_variable_get(var_name) }
         define_method("#{name}=".to_sym) do |value|
           instance_variable_set(var_name, value)
@@ -22,7 +22,15 @@ module Accessors
     end
 
     def strong_attr_accessor(attr, attr_class)
-      
+      attr_name = "@#{attr}".to_sym
+      define_method(attr) { instance_variable_get(attr_name) }
+      if attr_class == self.class.attr
+        define_method("#{attr}=".to_sym) do |value|
+          instance_variable_set(attr_name, value)
+        end
+      else
+        raise "Wrong attribute class!"
+      end
     end
 
   module InstanceMethods 
--- Desktop/home_work/Lesson_9/validation.rb
+++ Desktop/home_work/Lesson_9/validation.rb
@@ -1,6 +1,19 @@
 module Validation
 
-  def self.validate
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods 
+
+    def validate(*attr_name, *validation_type)
+      # ####
+    end
+
+  end
+
+  module InstanceMethods
   end
 
 end
