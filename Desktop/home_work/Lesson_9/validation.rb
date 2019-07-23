5577289344f3f94a99f8b3069eca486a1f711eda
dtrift <89174579997@mail.ru>
2019-07-23 22:09:07 +0500

    Lesson_9_4

--- Desktop/home_work/Lesson_9/validation.rb
+++ Desktop/home_work/Lesson_9/validation.rb
@@ -7,13 +7,45 @@ module Validation
 
   module ClassMethods 
 
-    def validate(*attr_name, *validation_type)
-      # ####
+    def validate(attr_name, validation_type, other_attr = nil)
+      @attr_name = attr_name
+      @validation_type = validation_type
+      @other_attr = other_attr
     end
 
   end
 
   module InstanceMethods
+
+    def valid?
+      validate!
+      true
+    rescue StandardError
+      false
+    end
+
+    protected
+
+    # validate :name, :presence
+    # validate :number, :format, /A-Z{0,3}/
+    # validate :station, :type, RailwayStation
+
+    def presence
+      raise 'Name can not be nil!' if @attr_name.empty? || @attr_name.nil?
+    end
+
+    def format
+      raise 'Wrong format!' if @validation_type !~ @other_attr
+    end
+
+    def type
+      raise 'Wrong type!' if @validation_type.class != @other_attr
+    end
+
+    def validate!
+
+    end
+
   end
 
 end
