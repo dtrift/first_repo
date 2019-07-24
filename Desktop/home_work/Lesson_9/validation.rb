4a87da2ea1d0fa37b183cdef8e89a382c4a8dbe9
dtrift <89174579997@mail.ru>
2019-07-24 23:16:09 +0500

    Lesson_9_4_2

--- Desktop/home_work/Lesson_9/validation.rb
+++ Desktop/home_work/Lesson_9/validation.rb
@@ -7,37 +7,43 @@ module Validation
 
   module ClassMethods 
 
-    def validate(attr_name, validation, other = nil)
+    def validate(attr_name, validation_type, *params = nil)
       @validations ||= []
-      @validations << [attr_name, validation, other]
-    # @validations << {} - или лучше использовать массив хэшей?
+      @validations << { validation => { attr_name: attr_name, params: params } }
     end
 
     def presence(attr_name)
       raise 'Name can not be nil!' if attr_name.empty? || attr_name.nil?
     end
 
-    def format(validation, other)
-      raise 'Wrong format!' if validation !~ other
+    def format(validation_type, params)
+      raise 'Wrong format!' if validation_type !~ params
     end
 
-    def type(attr_name, other)
-      raise 'Wrong type!' if validation.class != other
+    def type(attr_name, params)
+      raise 'Wrong type!' if validation_type.class != params
     end
 
   end
 
+  # validate :name, :presence
+  # validate :name, :format, /A-Z/
+  # validate :name, :type, String
+
   module InstanceMethods
 
     def validate!
-      # Перебрать массив массивов @validations  
+      #
+      # Перебрать массив хешей @validations  
       # Получить значение первого элемента вложенного массива и 
       # сравнить его с условием второго элемента вложенного массива и
       # 
-      @validations.each do |validation|
-        attr_name = validation[0]
-        validation = validation[1]
-        other = validation[2]
+      # 1. Получить значение валидируемой переменной - instance_variable_get
+      # 2. Cобрать название вызываемого метода валидации (например, validate_format)
+      # 3. Cделать send на вызываемый метод
+      #
+      @validations.each do |attr_name, validation_type|
+        
       end
      
     end
