61cf5d3d343bc514ac9e03af418729c9926a3b5b
dtrift <89174579997@mail.ru>
2019-07-29 20:11:31 +0500

    Lesson_9_5_4

--- Desktop/home_work/Lesson_9/validation.rb
+++ Desktop/home_work/Lesson_9/validation.rb
@@ -3,38 +3,23 @@ module Validation
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
-    # base.validations = []
+    base.validations = []
   end
 
   module ClassMethods 
-    attr_reader :validations
+    attr_accessor :validations
 
     def validate(attr_name, validation_type, params = nil)
       @validations ||= []
       @validations << { attr_name: attr_name, validation_type: validation_type, params: params }
     end
-
-    # def validate_presence(attr_name)
-    #   raise 'Name can not be nil!' if attr_name.empty? || attr_name.nil?
-    # end
-
-    # def validate_format(validation_type, params)
-    #   raise 'Wrong format!' if validation_type !~ params
-    # end
-
-    # def validate_type(attr_name, params)
-    #   raise 'Wrong type!' if validation_type.class != params
-    # end
   end
 
   module InstanceMethods
 
     def validate!
-      self.class.validations.to_a.each do |attr_name, validation_type, params|
-        var_name = "@#{attr_name}".to_sym
-        define_method(attr_name) { instance_variable_get(var_name) }
-        method_name = "validate_#{validation_type}".to_sym
-        send(method_name, instance_variable_get(var_name, params))
+      self.class.validations.each do |validation|
+        puts validation
       end    
     end
 
@@ -60,3 +45,11 @@ module Validation
     end
   end
 end
+
+
+        # # validation.each do |key, value|
+        # #   var_name = "@#{key}".to_sym
+        # #   define_method(key) { instance_variable_get(var_name) }
+        # #   method_name = "validate_#{validation_type}".to_sym
+        # #   send(method_name, instance_variable_get(var_name, value))
+        # end
