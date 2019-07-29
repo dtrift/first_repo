4844fceebd3187d8556fb4c83524fe8b16c04b54
dtrift <89174579997@mail.ru>
2019-07-29 23:21:50 +0500

    Lesson_9_5_6

--- Desktop/home_work/Lesson_9/validation.rb
+++ Desktop/home_work/Lesson_9/validation.rb
@@ -3,24 +3,42 @@ module Validation
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
-    # base.validations = []
+    base.validations = []
   end
 
   module ClassMethods 
     attr_accessor :validations
 
     def validate(attr_name, validation_type, params = nil)
-      validations ||= []
-      validations << { attr_name: attr_name, validation_type: validation_type, params: params }
+      @validations ||= []
+      @validations << { attr_name: attr_name, validation_type: validation_type, params: params }
     end
+
+    # def validate_presence(attr_name)
+    #   raise 'Name can not be nil!' if attr_name.empty? || attr_name.nil?
+    # end
+
+    # def validate_format(validation_type, params)
+    #   raise 'Wrong format!' if validation_type !~ params
+    # end
+
+    # def validate_type(attr_name, params)
+    #   raise 'Wrong type!' if validation_type.class != params
+    # end
   end
 
   module InstanceMethods
   
     def validate!
-      self.class.validations.each do |validation|
-        puts validation
-      end    
+      puts "@validations - #{self.class.validations} nil"
+      # self.class.validations.each do |validation|
+      #   validation.each do |name, params|
+      #     attr_name = "@#{name}"
+      #     define_method(name) { instance_variable_get(attr_name) }
+      #     method_name = "validate_#{params}"
+      #     send(method_name, instance_variable_get(name, params))
+      #   end
+      # end    
     end
 
     def valid?
@@ -53,3 +71,10 @@ end
         # #   method_name = "validate_#{validation_type}".to_sym
         # #   send(method_name, instance_variable_get(var_name, value))
         # end
+
+        # var_name = "@#{attr_name}".to_sym
+        # define_method(attr_name) { instance_variable_get(var_name) }
+        # method_name = "validate_#{validation_type}".to_sym
+        # send(method_name, instance_variable_get(var_name, params))
+
+
