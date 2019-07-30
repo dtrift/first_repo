647edf389f2ee925687f7888a1d959f35afbea7e
dtrift <89174579997@mail.ru>
2019-07-30 20:17:16 +0500

    Lesson_9_6

--- Desktop/home_work/Lesson_9/validation.rb
+++ Desktop/home_work/Lesson_9/validation.rb
@@ -14,31 +14,16 @@ module Validation
       @validations << { attr_name: attr_name, validation_type: validation_type, params: params }
     end
 
-    # def validate_presence(attr_name)
-    #   raise 'Name can not be nil!' if attr_name.empty? || attr_name.nil?
-    # end
-
-    # def validate_format(validation_type, params)
-    #   raise 'Wrong format!' if validation_type !~ params
-    # end
-
-    # def validate_type(attr_name, params)
-    #   raise 'Wrong type!' if validation_type.class != params
-    # end
   end
 
   module InstanceMethods
   
     def validate!
-      puts "@validations - #{self.class.validations} nil"
-      # self.class.validations.each do |validation|
-      #   validation.each do |name, params|
-      #     attr_name = "@#{name}"
-      #     define_method(name) { instance_variable_get(attr_name) }
-      #     method_name = "validate_#{params}"
-      #     send(method_name, instance_variable_get(name, params))
-      #   end
-      # end    
+      self.class.validations.each do |validation|
+        var_name = instance_variable_get("@#{validation[:attr_name]}")
+        method_name = validation[:validation_type]
+        send(method_name, var_name, *validation[:params])
+      end    
     end
 
     def valid?
@@ -50,31 +35,16 @@ module Validation
 
     protected
 
-    def validate_presence(attr_name)
+    def presence(attr_name)
       raise 'Name can not be nil!' if attr_name.empty? || attr_name.nil?
     end
 
-    def validate_format(validation_type, params)
+    def format(validation_type, params)
       raise 'Wrong format!' if validation_type !~ params
     end
 
-    def validate_type(attr_name, params)
+    def type(attr_name, params)
       raise 'Wrong type!' if validation_type.class != params
     end
   end
 end
-
-
-        # # validation.each do |key, value|
-        # #   var_name = "@#{key}".to_sym
-        # #   define_method(key) { instance_variable_get(var_name) }
-        # #   method_name = "validate_#{validation_type}".to_sym
-        # #   send(method_name, instance_variable_get(var_name, value))
-        # end
-
-        # var_name = "@#{attr_name}".to_sym
-        # define_method(attr_name) { instance_variable_get(var_name) }
-        # method_name = "validate_#{validation_type}".to_sym
-        # send(method_name, instance_variable_get(var_name, params))
-
-
